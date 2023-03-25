171230fbe88eca85f5b0a9fed3bf0f142725afdc
Mark Washburn <mark@mrwconsulting.tech>
2023-03-25 17:28:28 -0500

    chore: sample cleanup

--- shinto-manager/bin/shinto-manager.ts
+++ shinto-manager/bin/shinto-manager.ts
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
-import { RegionType } from '@mrwconsulting/shinto';
-import { ControlHubProps,ShintoManagerApi } from '@mrwconsulting/shinto';
+import { ShintoManagerApi } from '@mrwconsulting/shinto-api';
 import 'source-map-support/register';
 
-const props: ControlHubProps = {
-  account: "570602336658",
-  region: RegionType.US_EAST_1
-}
-const api = new ShintoManagerApi(props)
+const api = new ShintoManagerApi()
 api.stack()
