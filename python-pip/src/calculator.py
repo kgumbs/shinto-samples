ead83a2ceb36975e91fd30ff53c0a6b42c40f8c9
Mark Washburn <mark@mrwconsulting.tech>
2023-05-19 13:03:50 -0500

    chore: updated python-pip

--- python-pip/src/calculator.py
+++
@@ -1,5 +0,0 @@
-def add(first_term: int, second_term: int) -> int:
-    return first_term + second_term
-
-def subtract(first_term: int, second_term: int) -> int:
-    return first_term - second_term
