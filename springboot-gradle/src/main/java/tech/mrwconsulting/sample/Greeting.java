d2abd88e5bc1dc652dea0d8f0bea063cb48f6592
Mark Washburn <mark@mrwconsulting.tech>
2023-05-19 12:25:20 -0500

    chore: fixced springboot gradle

---
+++ springboot-gradle/src/main/java/tech/mrwconsulting/sample/Greeting.java
@@ -0,0 +1,3 @@
+package tech.mrwconsulting.sample;
+
+public record Greeting(long id, String content) { }
---
+++ springboot-gradle/src/main/java/tech/mrwconsulting/sample/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.mrwconsulting.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
