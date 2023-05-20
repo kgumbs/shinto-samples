12621f059b8246e6d98dc5e77f8a23b4f1822b6e
Mark Washburn <mark@mrwconsulting.tech>
2023-05-20 10:56:46 -0500

    chore: poetry cleanup

--- python-poetry/helloworld/model.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class User:
-    name: str
-
-    @property
-    def name_upper(self):
-        return self.name.upper()
--- python-poetry/tests/test_user.py
+++
@@ -1,6 +0,0 @@
-from helloworld.model import User
-
-def test_user():
-    user = User("foo")
-    assert user.name == "foo"
-    assert user.name_upper == "FOO"
