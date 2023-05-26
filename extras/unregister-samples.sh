4d9b76c7affa0d0c30506517ed5c2f9c9bbb6fcc
Mark Washburn <mark@mrwconsulting.tech>
2023-05-26 13:42:56 -0500

    chore: updated extra scripts

--- extras/unregister-samples.sh
+++ extras/unregister-samples.sh
@@ -9,8 +9,13 @@ fi
 declare -a LIST=("jib-gradle" "jib-maven" "python-pip" "python-poetry" "springboot-gradle" "springboot-kotlin" "springboot-maven")
 
 DEFAULT_DIR=$(PWD)/..
+DEFAULT_SLEEPTIME=15
 read -e -p  "Parent directory: [$DEFAULT_DIR] " PARENT_DIR
 PARENT_DIR="${PARENT_DIR:-$DEFAULT_DIR}"
+read -e -p  "Sleep time between samples: [$DEFAULT_SLEEPTIME] " SLEEPTIME
+SLEEPTIME="${SLEEPTIME:-$DEFAULT_SLEEPTIME}"
 for INDEX in "${LIST[@]}"; do
     shinto-manager unregister $PARENT_DIR/$INDEX/.shinto/props.json
+    echo "pausing between unregistering...${SLEEPTIME} seconds"
+    sleep ${SLEEPTIME}
 done
