84dd71ad908f5fcef2309015b56c9611927efb6f
Mark Washburn <mark@mrwconsulting.tech>
2023-05-26 08:57:22 -0500

    chore: added support scripts

---
+++ extras/unregister-samples.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+set +x
+set -o pipefail
+if [ "${DEBUG}" == "true" ]; then
+    set -x
+fi
+
+declare -a LIST=("jib-gradle" "jib-maven" "python-pip" "python-poetry" "springboot-gradle" "springboot-kotlin" "springboot-maven")
+
+DEFAULT_DIR=$(PWD)/..
+read -e -p  "Parent directory: [$DEFAULT_DIR] " PARENT_DIR
+PARENT_DIR="${PARENT_DIR:-$DEFAULT_DIR}"
+for INDEX in "${LIST[@]}"; do
+    shinto-manager unregister $PARENT_DIR/$INDEX/.shinto/props.json
+done
