5a83e17f944791417c43dd217baf575e3768af41
Mark Washburn <mark@mrwconsulting.tech>
2023-06-05 16:23:48 -0500

    chore: updated poetry unit-test

--- python-poetry/tests/test_helloworld.py
+++ python-poetry/tests/test_helloworld.py
@@ -1,16 +1,9 @@
 import pytest
 from helloworld.helloworld import app as flask_app
 
-@pytest.fixture
-def app():
-    yield flask_app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
 def test_index_route():
-    response = client().get('/')
+    client=flask_app.test_client()
+    response=client().get('/')
 
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'Hello, World!'
